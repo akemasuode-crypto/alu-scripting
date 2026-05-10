59c5fa12b2e18263f80641e664cb8b634bd5c5ff
Annyk <a.kemasuode@alustudent.com>
2026-05-10 06:21:22 -0700

    Fix all failing solutions

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.+)\] \[to:(.+)\] \[flags:(.+)\]/).map do |sender, receiver, flags|
-  "#{sender},#{receiver},#{flags}"
+puts ARGV[0].scan(/\[from:(.+)\] \[to:(.+)\] \[flags:(.+)\]/).map do |s, r, f|
+  "#{s},#{r},#{f}"
 end.join
